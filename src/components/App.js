f3ba3e11bf1c22a7e8c1060214bee5effb76fe4a
Rithikeshh <135729296+Rithikeshh@users.noreply.github.com>
2023-12-17 09:41:37 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,21 @@
 import React,{useState,useEffect} from 'react'
 import '../styles/App.css';
 const App = () => {
-
+  const [valu1, setValue1] = useState('');
+  const [valu2, setValue2] = useState('');
+  const [result, setResult] = useState(0);
+  useEffect(()=>{
+    setResult(parseInt(value1)+parseInt(value2)}
+  },[value1,value2])
   return (
     <div id="main">
-      <input id='input1' onChange={}/>
+      <input id='input1' value={value2} onChange={(e)=>{
+    setValue1(e.target.value)
+      }}/>
        +
-      <input id='input2' onChange={}/> 
+      <input id='input2' value={value2} onChange={(e)=>{
+    setValue2(e.target.value)
+      }}/> 
       
       <p id='result'></p>
     </div>
